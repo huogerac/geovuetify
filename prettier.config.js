2c0c234dcc072ed18850353098f8acc2bb2228dc
joaoprocopio <joaovitorcprocopio@gmail.com>
2024-12-18 20:56:54 -0300

    chore: adiciona e configura o eslint

---
+++ prettier.config.js
@@ -0,0 +1,13 @@
+/** @type {import("prettier").Config} */
+const config = {
+  semi: false,
+  singleQuote: false,
+  bracketSameLine: true,
+  vueIndentScriptAndStyle: true,
+  trailingComma: "all",
+  quoteProps: "consistent",
+  printWidth: 100,
+  // plugins: ["prettier-plugin-tailwindcss"],
+}
+
+export default config;
