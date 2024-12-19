cb2982d57fc3cb633bf3280acc90409aaeb5c67d
joaoprocopio <joaovitorcprocopio@gmail.com>
2024-12-18 21:45:01 -0300

    chore: reorganiza configs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,12 +3,19 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   srcDir: "src",
-  modules: ["@nuxt/eslint", "@nuxtjs/tailwindcss", "@nuxtjs/color-mode", "shadcn-nuxt"],
+  modules: ["radix-vue/nuxt", "@nuxt/eslint", "@nuxtjs/tailwindcss", "@nuxtjs/color-mode"],
   tailwindcss: {
     cssPath: "~/assets/styles/tailwind.css",
+    viewer: false,
   },
-  shadcn: {
-    prefix: "Nyx",
-    componentDir: "./src/components/nyx",
+  radix: {
+    prefix: "internal",
   },
+  components: [
+    {
+      path: "~/components/nyx",
+      extensions: [".vue"],
+      prefix: "nyx",
+    },
+  ],
 })
