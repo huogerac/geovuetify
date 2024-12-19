239e2d4998ad33daf2951e8dcbb1b4c2fdb75311
joaoprocopio <joaovitorcprocopio@gmail.com>
2024-12-18 21:24:07 -0300

    chore: adiciona módulo nuxt do shadcn

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,8 +3,12 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   srcDir: "src",
-  modules: ["@nuxt/eslint", "@nuxtjs/tailwindcss"],
+  modules: ["@nuxt/eslint", "@nuxtjs/tailwindcss", "@nuxtjs/color-mode", "shadcn-nuxt"],
   tailwindcss: {
     cssPath: "~/assets/styles/tailwind.css",
+  },
+  shadcn: {
+    prefix: "Nyx",
+    componentDir: "./src/components/nyx",
   },
 })
