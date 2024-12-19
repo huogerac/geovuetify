3c69e65d0aea2d54ee8a8af5c5380e1a4a7d2ca9
joaoprocopio <joaovitorcprocopio@gmail.com>
2024-12-18 22:55:13 -0300

    feat: adiciona html title

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,10 +3,39 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   srcDir: "src",
-  modules: ["radix-vue/nuxt", "@nuxt/eslint", "@nuxtjs/tailwindcss", "@nuxtjs/color-mode"],
+  modules: [
+    "radix-vue/nuxt",
+    "@nuxt/eslint",
+    // "@nuxt/fonts",
+    // "@nuxt/icon",
+    // @vueuse/nuxt,
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/color-mode",
+  ],
+  app: {
+    head: {
+      title: "Generate your SBTN report",
+      titleTemplate: "%s | opendata4nature",
+      htmlAttrs: {
+        lang: "en",
+      },
+      meta: [
+        {
+          charset: "utf-8",
+        },
+        {
+          name: "viewport",
+          content: "width=device-width, initial-scale=1",
+        },
+      ],
+    },
+  },
   tailwindcss: {
     cssPath: "~/assets/styles/tailwind.css",
     viewer: false,
+  },
+  colorMode: {
+    classSuffix: "",
   },
   radix: {
     prefix: "internal",
