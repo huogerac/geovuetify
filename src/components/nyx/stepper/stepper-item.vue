995f10ecfce12e788d2c6032db77af13d771c9b9
joaoprocopio <joaovitorcprocopio@gmail.com>
2024-12-19 20:49:24 -0300

    feat: adiciona o componente stepper

---
+++ src/components/nyx/stepper/stepper-item.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+  import type { StepperItemProps } from "radix-vue"
+  import { useForwardProps } from "radix-vue"
+  import type { HTMLAttributes } from "vue"
+
+  const props = defineProps<StepperItemProps & { class?: HTMLAttributes["class"] }>()
+
+  const delegatedProps = computed(() => {
+    const { class: _, ...delegated } = props
+
+    return delegated
+  })
+
+  const forwarded = useForwardProps(delegatedProps)
+</script>
+
+<template>
+  <StepperItem
+    v-slot="slotProps"
+    v-bind="forwarded"
+    :class="cn('group flex items-center gap-2 data-[disabled]:pointer-events-none', props.class)">
+    <slot v-bind="slotProps" />
+  </StepperItem>
+</template>
---
+++ src/components/nyx/stepper/stepper-trigger.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+  import type { StepperTriggerProps } from "radix-vue"
+  import { useForwardProps } from "radix-vue"
+  import type { HTMLAttributes } from "vue"
+
+  const props = defineProps<StepperTriggerProps & { class?: HTMLAttributes["class"] }>()
+
+  const delegatedProps = computed(() => {
+    const { class: _, ...delegated } = props
+
+    return delegated
+  })
+
+  const forwarded = useForwardProps(delegatedProps)
+</script>
+
+<template>
+  <StepperTrigger
+    v-bind="forwarded"
+    :class="cn('flex flex-col items-center gap-2 rounded-md p-2 text-center', props.class)">
+    <slot />
+  </StepperTrigger>
+</template>
