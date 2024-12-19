569bbacffd77f67e9c5e31a4e44cba5f5ced6ccf
joaoprocopio <joaovitorcprocopio@gmail.com>
2024-12-19 00:10:47 -0300

    feat: faz a versão 1

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,11 +1,56 @@
 <template>
-  <div>
-    <h1>default layout</h1>
+  <NyxSidebarProvider :default-open="false">
+    <NyxSidebar collapsible="icon">
+      <NyxSidebarHeader>
+        <NyxSidebarMenu>
+          <NyxSidebarMenuItem>
+            <NyxSidebarMenuButton size="lg">
+              <div
+                class="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
+                <Globe class="size-4" />
+              </div>
 
-    <NyxButton variant="destructive">abc</NyxButton>
+              <div class="flex flex-col gap-0.5 leading-none">
+                <span class="font-semibold">opendata4nature</span>
+                <span>Building a better world!</span>
+              </div>
+            </NyxSidebarMenuButton>
+          </NyxSidebarMenuItem>
+        </NyxSidebarMenu>
+      </NyxSidebarHeader>
+    </NyxSidebar>
 
-    <main>
-      <slot />
-    </main>
-  </div>
+    <NyxSidebarInset>
+      <header
+        class="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+        <div class="flex items-center gap-2 px-4">
+          <NyxSidebarTrigger class="-ml-1" />
+          <!-- <Separator orientation="vertical" class="mr-2 h-4" />
+          <Breadcrumb>
+            <BreadcrumbList>
+              <BreadcrumbItem class="hidden md:block">
+                <BreadcrumbLink href="#"> Building Your Application </BreadcrumbLink>
+              </BreadcrumbItem>
+              <BreadcrumbSeparator class="hidden md:block" />
+              <BreadcrumbItem>
+                <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+              </BreadcrumbItem>
+            </BreadcrumbList>
+          </Breadcrumb> -->
+        </div>
+      </header>
+      <div class="flex flex-1 flex-col gap-4 p-4 pt-0">
+        <div class="grid auto-rows-min gap-4 md:grid-cols-3">
+          <div class="aspect-video rounded-xl bg-muted/50" />
+          <div class="aspect-video rounded-xl bg-muted/50" />
+          <div class="aspect-video rounded-xl bg-muted/50" />
+        </div>
+        <div class="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" />
+      </div>
+    </NyxSidebarInset>
+  </NyxSidebarProvider>
 </template>
+
+<script setup lang="ts">
+  import { Globe } from "lucide-vue-next"
+</script>
