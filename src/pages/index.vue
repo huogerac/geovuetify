ee61a87c003b4c4d1ba37dfa705c260567e13523
joaoprocopio <joaovitorcprocopio@gmail.com>
2024-12-19 21:33:43 -0300

    feat: adiciona a primeira tab

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="grid h-full grid-cols-[36rem,1fr]">
+  <div class="grid h-full grid-cols-[38rem,1fr]">
     <aside class="border-r">
       <NyxStepper class="border-b px-6 py-4">
         <NyxStepperItem v-for="step in steps" :key="step.id" :step="step.id">
@@ -22,6 +22,76 @@
           <NyxStepperSeparator v-if="step.id !== steps[steps.length - 1].id" class="h-px w-full" />
         </NyxStepperItem>
       </NyxStepper>
+
+      <div class="px-6 py-8">
+        <div>
+          <NyxTypographyH2>Template</NyxTypographyH2>
+          <NyxTypographyLead>
+            Download and fill the geolocations template spreadsheet
+          </NyxTypographyLead>
+        </div>
+
+        <div class="pt-8">
+          <NyxTypographyP>
+            In this step, you will need to download and fill the geolocations template spreadsheet.
+          </NyxTypographyP>
+
+          <NyxTypographyP>
+            Inside this template spreadsheet you will find the following fields:
+          </NyxTypographyP>
+
+          <NyxTypographyTableContainer>
+            <NyxTypographyTable class="table-fixed">
+              <NyxTypographyTableThead>
+                <NyxTypographyTableRow>
+                  <NyxTypographyTableHead>Field name</NyxTypographyTableHead>
+                  <NyxTypographyTableHead>Field description</NyxTypographyTableHead>
+                </NyxTypographyTableRow>
+              </NyxTypographyTableThead>
+
+              <NyxTypographyTableTbody>
+                <NyxTypographyTableRow>
+                  <NyxTypographyTableData>Company</NyxTypographyTableData>
+                  <NyxTypographyTableData>Enter the company name</NyxTypographyTableData>
+                </NyxTypographyTableRow>
+
+                <NyxTypographyTableRow>
+                  <NyxTypographyTableData>Site</NyxTypographyTableData>
+                  <NyxTypographyTableData>Enter the site name</NyxTypographyTableData>
+                </NyxTypographyTableRow>
+
+                <NyxTypographyTableRow>
+                  <NyxTypographyTableData>ISIC</NyxTypographyTableData>
+                  <NyxTypographyTableData>
+                    Enter the ISIC Group Code, you can find it on the spreadsheet
+                  </NyxTypographyTableData>
+                </NyxTypographyTableRow>
+
+                <NyxTypographyTableRow>
+                  <NyxTypographyTableData>Latitude</NyxTypographyTableData>
+                  <NyxTypographyTableData>
+                    Enter the latitude of the site (in WGS84 decimal degrees)
+                  </NyxTypographyTableData>
+                </NyxTypographyTableRow>
+
+                <NyxTypographyTableRow>
+                  <NyxTypographyTableData>Longitude</NyxTypographyTableData>
+                  <NyxTypographyTableData>
+                    Enter the longitude of the site (in WGS84 decimal degrees).
+                  </NyxTypographyTableData>
+                </NyxTypographyTableRow>
+              </NyxTypographyTableTbody>
+            </NyxTypographyTable>
+          </NyxTypographyTableContainer>
+
+          <div class="mx-auto max-w-36">
+            <NyxButton class="w-full">
+              <span>Download</span>
+              <ArrowDownToLine class="size-4" />
+            </NyxButton>
+          </div>
+        </div>
+      </div>
     </aside>
 
     <section>abc</section>
@@ -29,7 +99,7 @@
 </template>
 
 <script setup lang="ts">
-  import { FileUp, HardDriveDownload, Sheet } from "lucide-vue-next"
+  import { ArrowDownToLine, FileUp, HardDriveDownload, Sheet } from "lucide-vue-next"
 
   const steps = [
     {
