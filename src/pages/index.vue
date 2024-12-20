ed5ff9b394cc8a2981a39781cb6372a28dd7f40d
joaoprocopio <joaovitorcprocopio@gmail.com>
2024-12-19 21:14:48 -0300

    feat: melhora o layout

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,26 +1,30 @@
 <template>
-  <div>
-    <NyxStepper>
-      <NyxStepperItem v-for="step in steps" :key="step.id" :step="step.id">
-        <NyxStepperTrigger>
-          <NyxStepperIndicator>
-            <component :is="step.icon" class="size-4" />
-          </NyxStepperIndicator>
+  <div class="grid h-full grid-cols-[36rem,1fr]">
+    <aside class="border-r">
+      <NyxStepper class="border-b px-6 py-4">
+        <NyxStepperItem v-for="step in steps" :key="step.id" :step="step.id">
+          <NyxStepperTrigger>
+            <NyxStepperIndicator>
+              <component :is="step.icon" class="size-4" />
+            </NyxStepperIndicator>
 
-          <div class="flex flex-col">
-            <NyxStepperTitle>
-              {{ step.title }}
-            </NyxStepperTitle>
+            <div class="flex flex-col">
+              <NyxStepperTitle>
+                {{ step.title }}
+              </NyxStepperTitle>
 
-            <NyxStepperDescription>
-              {{ step.description }}
-            </NyxStepperDescription>
-          </div>
-        </NyxStepperTrigger>
+              <NyxStepperDescription>
+                {{ step.description }}
+              </NyxStepperDescription>
+            </div>
+          </NyxStepperTrigger>
 
-        <NyxStepperSeparator v-if="step.id !== steps[steps.length - 1].id" class="h-px w-full" />
-      </NyxStepperItem>
-    </NyxStepper>
+          <NyxStepperSeparator v-if="step.id !== steps[steps.length - 1].id" class="h-px w-full" />
+        </NyxStepperItem>
+      </NyxStepper>
+    </aside>
+
+    <section>abc</section>
   </div>
 </template>
 
