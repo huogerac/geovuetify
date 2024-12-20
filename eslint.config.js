ca25a33a160a8627bfed7265745c815ac97ea670
joaoprocopio <joaovitorcprocopio@gmail.com>
2024-12-19 21:02:23 -0300

    chore: adiciona import sort

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,21 @@
 import prettier from "eslint-plugin-prettier/recommended"
+import simpleImportSort from "eslint-plugin-simple-import-sort"
 import withNuxt from "./.nuxt/eslint.config.mjs"
 
-const config = withNuxt(prettier, {
-  ignores: ["node_modules/**/*", "lib/**/*"],
-})
+const config = withNuxt(
+  prettier,
+  {
+    ignores: ["node_modules/**/*", "lib/**/*"],
+  },
+  {
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+    },
+  },
+)
 
 export default config
